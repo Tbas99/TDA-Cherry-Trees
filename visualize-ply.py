1de4e528c83c30e36b8a5e47c9a0c72838794249
Luuk Spierings <spierings.luuk68@gmail.com>
2023-06-19 14:19:20 +0200

    Further progress

--- visualize-ply.py
+++ visualize-ply.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 import gudhi as gd
 from open3d import io, visualization
 
+
 def main():
     # Path
     pathToData = os.path.join(os.getcwd(), 'data', 'bags', 'bag_0')
 
     # Read Data
-    pointCloud = io.read_point_cloud(os.path.join(pathToData, 'cloud_final.ply')) # Read point cloud
+    pointCloud = io.read_point_cloud(os.path.join(
+        pathToData, 'cloud_final.ply'))  # Read point cloud
     print(pointCloud)
     pointCloudArray = np.asarray(pointCloud.points)
 
     # Visualize point cloud
-    visualize = False
+    visualize = True
     if visualize:
         visualization.draw_geometries([pointCloud])
 
     # # Create alpha complex
-    alphaComplex = gd.AlphaComplex(points = pointCloudArray)
+    alphaComplex = gd.AlphaComplex(points=pointCloudArray)
 
     # # Create simplex tree object
     simplexTree = alphaComplex.create_simplex_tree()
@@ -28,6 +30,7 @@ def main():
     persistenceDiagram = simplexTree.persistence()
     gd.plot_persistence_diagram(persistenceDiagram, legend=True)
     plt.show()
+
 
 if __name__ == "__main__":
     main()
